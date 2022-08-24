4bdcc293b24fce4c2a4549136d478a69a28d5c99
Simon Vargas <sjvargas01@gmail.com>
2022-08-24 00:05:45 -0400

    updated user-fomr, user-list, and app routing files

---
+++ src/app/user-list/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user-service.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users!: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.findAll().subscribe(data => {
+      this.users = data;
+    });
+  }
+
+}
